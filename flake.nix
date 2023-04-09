b3bf50c6404e19181ee08f10c670365966f300d0
Max Hausch <github@hausch.xyz>
2023-04-09 11:43:52 +0200

    feat(generators): add iso, vm and vagrant-virtualbox generators

--- flake.nix
+++ flake.nix
@@ -1,15 +1,44 @@
 {
   description = "Flake for the precice-Nix research project";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-  inputs.home-manager.url = "github:nix-community/home-manager/release-22.11";
-  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-22.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-  outputs = { self, nixpkgs, home-manager }: {
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-    nixosConfigurations.precice-vm = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, home-manager, nixos-generators }: let
+    precice-system = {
       system = "x86_64-linux";
       modules = [ home-manager.nixosModules.home-manager ./configuration.nix ];
+    };
+    precice-system-virtualbox = precice-system // {
+      modules = precice-system.modules ++ [{
+        virtualbox = {
+          memorySize = 2048;
+          vmName = "preCICE-VM";
+          params = {
+            cpus = 2;
+            vram = 64;
+            graphicscontroller = "vmsvga"; # This is default
+          };
+        };
+      }];
+    };
+  in {
+    nixosConfigurations.precice-vm = nixpkgs.lib.nixosSystem precice-system;
+
+    packages.x86_64-linux = {
+      iso = nixos-generators.nixosGenerate (precice-system // { format = "iso"; });
+      vagrant-vbox-image = nixos-generators.nixosGenerate (precice-system-virtualbox // { format = "vagrant-virtualbox"; });
+      vm = nixos-generators.nixosGenerate (precice-system // { format = "vm"; });
     };
   };
 }
