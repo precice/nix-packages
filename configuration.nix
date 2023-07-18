precice/nix-packages

commit ceee58fb0815db2f819ff3c9689f6d842df06999

feat: Add way better Vagrant support

configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 {
   nixpkgs.overlays = lib.mkBefore (import ./precice-packages);
   nixpkgs.config = {
@@ -89,7 +89,30 @@
     };
   };
 
+  fileSystems."/" = {
+    device = "/dev/sda1";
+    fsType = "ext4";
+  };
+  boot.loader.grub.device = "/dev/sda";
+  # This is all needed to make resizing work inside the VirtualBox VM
   virtualisation.virtualbox.guest.enable = true;
+  services.xserver.videoDrivers = lib.mkForce [ "vmware" "virtualbox" "modesetting" ];
+  systemd.user.services = let
+    vbox-client = desc: flags: {
+      description = "VirtualBox Guest: ${desc}";
+
+      wantedBy = [ "graphical-session.target" ];
+      requires = [ "dev-vboxguest.device" ];
+      after = [ "dev-vboxguest.device" ];
+
+      unitConfig.ConditionVirtualization = "oracle";
+
+      serviceConfig.ExecStart = "${config.boot.kernelPackages.virtualboxGuestAdditions}/bin/VBoxClient -fv ${flags}";
+    };
+  in {
+    virtualbox-resize = vbox-client "Resize" "--vmsvga";
+    virtualbox-clipboard = vbox-client "Clipboard" "--clipboard";
+  };
 
   users.users.precice = {
     isNormalUser = true;

Vagrantfile
@@ -1,0 +1,31 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "preCICE"
+  config.vm.box_check_update = false
+
+  config.vm.provider "virtualbox" do |vb|
+    # Name of the machine
+    vb.name = "preCICE-VM"
+    # Display the VirtualBox GUI when booting the machine
+    vb.gui = true
+    # Number of cores
+    vb.cpus = 2
+    # Customize the amount of memory on the VM:
+    vb.memory = "2048"
+    # Video memory (the default may be too low for some applications)
+    vb.customize ["modifyvm", :id, "--vram", "64"]
+    # The default graphics controller is VboxSVGA. This seems to cause issues with auto-scaling.
+    # VMSVGA seems to work better.
+    vb.customize ["modifyvm", :id, "--graphicscontroller", "vmsvga"]
+  end
+
+  # The libvirt provider needs to be installed using "vagrant plugin install vagrant-libvirt"
+  config.vm.provider :libvirt do |lv|
+    lv.forward_ssh_port = true
+    lv.title = "preCICE-VM"
+    lv.cpus = 2
+    lv.memory = 2048
+  end
+end

commit e25ad897bbc7f9ed88372be25bd34b1d833e86b9

fix: Errors that were introduced in ceee58f

configuration.nix
@@ -90,7 +90,7 @@
   };
 
   fileSystems."/" = {
-    device = "/dev/sda1";
+    device = "/dev/disk/by-label/nixos";
     fsType = "ext4";
   };
   boot.loader.grub.device = "/dev/sda";

flake.nix
@@ -88,7 +88,7 @@
 
     # Access by running `nix develop`
     devShells.x86_64-linux.default = pkgs.mkShell {
-      buildInputs = (import ./configuration.nix { inherit pkgs; lib = nixpkgs.lib; }).environment.systemPackages;
+      buildInputs = (import ./configuration.nix { inherit pkgs; lib = nixpkgs.lib; config = null; }).environment.systemPackages;
 
       shellHook = ''
         source ${pkgs.openfoam}/bin/set-openfoam-vars